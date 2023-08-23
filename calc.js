f5a90bd78e4bcfb7fcde6cd515e4682779688295
haritha8304 <harithap205@gmail.com>
2023-08-23 12:55:40 +0530

    calc

---
+++ calc.js
@@ -0,0 +1,17 @@
+function displays(content){
+    result.value += content
+}
+function allClear(){
+    result.value=""
+}
+function exprEval(){
+    try{
+        result.value=eval(result.value)
+    }catch{
+        result.value="invalid expression!!"
+    }
+   
+}
+function removeLast(){
+    result.value=result.value.slice(0,-1)
+}
